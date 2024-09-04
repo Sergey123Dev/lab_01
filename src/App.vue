59d80b71a0b73ec3ce37f14b46ba57dceebc2f87
Sergey Neroda <neroda.sergey1998@itlab-studio.com>
2024-09-04 23:09:37 +0300

    change default templates

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
@@ -61,16 +55,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 
   nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup>
-  console.log(import.meta.env.VITE_API_URL);
+import { ref } from 'vue'
+
+// Створюємо реактивну змінну, яка зберігає стан відкриття/закриття пунктів
+const isOpen1 = ref(false)
+const isOpen2 = ref(false)
+
+// Функції для перемикання стану відкриття/закриття
+const toggleOpen1 = () => {
+  isOpen1.value = !isOpen1.value
+}
+
+const toggleOpen2 = () => {
+  isOpen2.value = !isOpen2.value
+}
 </script>
 
 <template>
-  <main>
-    <h1>This is an about page</h1>
-  </main>
+  <div>
+    <!-- Пункт 1 -->
+    <div>
+      <h1 @click="toggleOpen1" style="cursor: pointer;">Пункт 1 (натисни для відкриття)</h1>
+      <div v-if="isOpen1">
+        <p>Це вміст першого пункту. Він відкривається при натисканні.</p>
+      </div>
+    </div>
+
+    <!-- Пункт 2 -->
+    <div>
+      <h1 @click="toggleOpen2" style="cursor: pointer;">Пункт 2 (натисни для відкриття)</h1>
+      <div v-if="isOpen2">
+        <p>Це вміст другого пункту. Він також відкривається при натисканні.</p>
+      </div>
+    </div>
+  </div>
 </template>
